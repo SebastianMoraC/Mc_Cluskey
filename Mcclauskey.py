90cefc911d492a52ea2b98d777a30a826b17cbab
jd0202 <61396768+jd0202@users.noreply.github.com>
2020-03-10 21:39:42 -0500

    Update Mcclauskey.py

--- Mcclauskey.py
+++ Mcclauskey.py
@@ -1,10 +1,10 @@
-class nodo(object):  # objeto para armacenar tupla de binario y bumero decimal
 
-    def __init__(self, combinacion, decimal, validacion):
-        self.combinacion = combinacion
-        self.decimal = decimal
-        self.validacion = validacion
+class nodo(object): #objeto para armacenar tupla de binario y bumero decimal
 
+    def __init__(self,combinacion,decimal,validacion):
+        self.combinacion=combinacion
+        self.decimal=decimal
+        self.validacion=validacion
 
 def crear_matriz(matriz, columnas, filas):  # Funcion que crea matrices
     for i in range(filas):
@@ -16,13 +16,11 @@ def crear_matriz(matriz, columnas, filas):  # Funcion que crea matrices
                 matriz[i].append(0)
     return matriz
 
-
 def dibujar_matriz(matriz, columnas, filas):  # Funcion que dibuja matrices
     for i in range(filas):
         for j in range(columnas):
             print(matriz[i][j], end="")  # El end se utiliza para que no se haga cambio de linea al imprimir en pantalla
         print("")  # Con esta linea hago el cambio de columna
-
 
 def llenar_matriz(filas, columnas, matriz):  # Esta funcion llena las matrices de ceros y unos
     cantidad = filas // 2
@@ -42,7 +40,6 @@ def llenar_matriz(filas, columnas, matriz):  # Esta funcion llena las matrices d
         cantidad = cantidad // 2  # Divide la cantidad de 0 y 1 que entraran
         iteracion = 0
     return matriz
-
 
 def minterms(matriz, filas):
     matriz_nueva = []
@@ -65,94 +62,131 @@ def minterms(matriz, filas):
 
     return matriz_nueva
 
+def EMCclauskey(matriz,variables): # se ordenan los valores binarios por cantidad de unos presentes
+    Mcclauskey=list() #lista final donde estara la agrupacion por unos
+    tempcombinaciones=list() #lista temporal donde se almacenara cada grupo de unos 
+    tempcombinacion=list()
+    tempdecimal=list() #lista para los valores decimales Nota: en primera instancia sera un unico valor pereo mientras se ejecuta el rpoceso de mclauskey se obtendran mas valores
 
-def EMCclauskey(matriz, variables, filas):  # se ordenan los valores binarios por cantidad de unos presentes
-    Mcclauskey = list()  # lista final donde estara la agrupacion por unos
-    tempcombinaciones = list()  # lista temporal donde se almacenara cada grupo de unos
-    tempcombinacion = list()
-    tempdecimal = list()  # lista para los valores decimales Nota: en primera instancia sera un unico valor pereo mientras se ejecuta el rpoceso de mclauskey se obtendran mas valores
-
-    iteracion = 0  # se itera la cantidad de variables que hay ininiando desde 0
-    while iteracion <= variables:  # se utilizara como control para que se guarden los valores por cantidad de unos
-        for a in range(filas):  # se reorre primero por cada fila
-            count = 0  # contador de unos
-            t = range(1, variables)  # por cuestiones de funcionamiento es necesario esta asignacion
-            for b in t:  # recorre por columnas, se empieza desde la posicion 1 por el hehcho de que en la primera posicion esta  el numero representado en decimal
-                if matriz[a][b] == 1:  # condicional para contar
-                    count += 1
-                tempcombinacion.append(matriz[a][b])  # se guarda la combinacion binaria
+    iteracion=0 #se itera la cantidad de variables que hay iniciando desde 0
+    flag=0 # una bandera para saber si existen agrupaciones de unos adecuadas para guardas
+    while iteracion <= variables: # se utilizara como control para que se guarden los valores por cantidad de unos  
+        for a in range(len(matriz)): #se reorre primero por cada fila
+            flag=0
+            count=0 # contador de unos
+            t=range(1,variables) # por cuestiones de funcionamiento es necesario esta asignacion
+            for b in t:# recorre por columnas, se empieza desde la posicion 1 por el hehcho de que en la primera posicion esta  el numero representado en decimal
+                if matriz[a][b] == 1: # condicional para contar
+                    count+=1
+                tempcombinacion.append(matriz[a][b]) # se guarda la combinacion binaria
                 # como #"$"!#"$!#!$ se borra toda una fila de la matriz?"
-            if count == iteracion:  # se verifica si la cantidad de unso coincide con la cantidad que se esta trabajando actualmente
-                tempdecimal.append(matriz[a][0])  # se guarda el decimal
-                n = nodo(tempcombinacion, tempdecimal, 0)  # se crea un objeto con el decimal y el binario
-                tempcombinacion.clear()  # se vacia la lista para evitar ambiguedad
+            if count == iteracion: # se verifica si la cantidad de unso coincide con la cantidad que se esta trabajando actualmente
+                flag=1
+                tempdecimal.append(matriz[a][0]) # se guarda el decimal
+                n=nodo(tempcombinacion,tempdecimal,0) # se crea un objeto con el decimal y el binario
+                tempcombinaciones.append(n) # finalmente se guarda la combinacion de binarios en una lista
+                n=None
+            else:
+                tempcombinacion.clear() # se vacia la lista para evitar ambiguedad
                 tempdecimal.clear()
-                tempcombinaciones.append(n)  # finalmente se guarda la combinacion de binarios en una lista
-        Mcclauskey.append(tempcombinaciones)  # finalmente se guarda la lista de binarios en una lista
-        tempcombinaciones.clear()
-        iteracion += 1  # aumenta la iteraion con el fin de comprobar todas las cantidades de unos
+        #if flag == 1:
+            Mcclauskey.append(tempcombinaciones) # finalmente se guarda la lista de binarios en una lista
+            flag=0
+            tempcombinaciones.clear()
+        iteracion+=1 # aumenta la iteraion con el fin de comprobar todas las cantidades de unos
+    return Mcclauskey
 
+def p(matriz,variables): # se ordenan los valores binarios por cantidad de unos presentes
+    Mcclauskey=list() #lista final donde estara la agrupacion por unos
+    tempcombinaciones=list() #lista temporal donde se almacenara cada grupo de unos 
+    tempcombinacion=list()
+    tempdecimal=list() #lista para los valores decimales Nota: en primera instancia sera un unico valor pereo mientras se ejecuta el rpoceso de mclauskey se obtendran mas valores
+    n1=list()
+
+    iteracion=0 #se itera la cantidad de variables que hay iniciando desde 0
+    flag=0 # una bandera para saber si existen agrupaciones de unos adecuadas para guardas
+    while iteracion <= variables: # se utilizara como control para que se guarden los valores por cantidad de unos  
+        for a in range(len(matriz)): #se reorre primero por cada fila
+            flag=0
+            count=0 # contador de unos
+            t=range(1,variables) # por cuestiones de funcionamiento es necesario esta asignacion
+            for b in t:# recorre por columnas, se empieza desde la posicion 1 por el hehcho de que en la primera posicion esta  el numero representado en decimal
+                if matriz[a][b] == 1: # condicional para contar
+                    count+=1
+                tempcombinacion.append(matriz[a][b]) # se guarda la combinacion binaria
+                # como #"$"!#"$!#!$ se borra toda una fila de la matriz?"
+            if count == iteracion: # se verifica si la cantidad de unso coincide con la cantidad que se esta trabajando actualmente
+                flag=1
+                tempdecimal.append(matriz[a][0]) # se guarda el decimal
+                n1.append(tempcombinacion)
+                n1.append(tempdecimal)
+                n1.append(0)
+                tempcombinaciones.append(n1) # finalmente se guarda la combinacion de binarios en una lista
+            print(tempcombinaciones)
+            n1.clear()
+            tempcombinacion.clear() # se vacia la lista para evitar ambiguedad
+            tempdecimal.clear()
+            if flag == 1:
+                Mcclauskey.append(tempcombinaciones) # finalmente se guarda la lista de binarios en una lista
+                flag=0
+            tempcombinaciones.clear()
+        iteracion+=1 # aumenta la iteraion con el fin de comprobar todas las cantidades de unos
     return Mcclauskey
 
 
 def comparacionMC1(MCclauskey):
-    Mcclauskeycomplete = list()  # lista que contendra las agrupaciones de las combinaciones resultantes
-    tempMcclauskeycomplete = list()
-    tempcombinaciones = list()  # lista que tendra temporalmente cada agrupacion
-
-    templist1 = list()  # lista para tomar una agrupacion
-    templist2 = list()  # lista para tomar la siguiente agrupacion
-
-    i = 0
-    while len(MCclauskey) >= i + 1:  # comenzamos a sacar cada agrupacion de 1
-        templist1 = MCclauskey[i]  # tomamos una agrupacion de unos
-        templist2 = MCclauskey[i + 1]  # y luego tomamos la siguiente agrupacion
-
-        for a in templist1:  # e comienza a recorrer la primera agrupacion con le fin de sacar una elemento y compararlos con todos los de la agrupacion siguiente
-            for b in templist2:  # se recorre la segunda agrupacion para compararla completamente con el elemento de sacado anteiormente
-                if ((a.combinacion.count(1) + 1 == b.combinacion.count(1)) or (
-                        a.combinacion.count(1) == b.combinacion.count(
-                        1) + 1)):  # se consulta si estos dos elemetos tienen un bit de diferencia
-                    tempbinary = list()  # esta variable temporal se encargara de solamente guardar el binario
-                    tempdecimal = list()
-                    for c in range(len(
-                            a.combinacion)):  # se comienzan a recorrer ambos elementos con el fin de encontrar la posicion donde se encuentra el uno en comun
-                        if a.combinacion[c] != b.combinacion[
-                            c]:  # se consulta si precisamente la posisicon en cuestion contiene el cambio de bit
-                            tempbinary.append("-")  # se registra el cambio de bit
+    Mcclauskeycomplete=list() #matriz que contendra las agrupaciones de las combinaciones resultantes
+    tempMcclauskeycomplete=list()
+    tempcombinaciones=list() #lista que tendra temporalmente cada agrupacion
+    
+    templist1=list() #lista para tomar una agrupacion 
+    templist2=list() #lista para tomar la siguiente agrupacion
+    
+    i=0
+    while len(MCclauskey) >= i+1: # comenzamos a sacar cada agrupacion de 1
+        templist1=MCclauskey[i] # tomamos una agrupacion de unos
+        templist2=MCclauskey[i+1] #y luego tomamos la siguiente agrupacion
+        
+        for a in templist1: #e comienza a recorrer la primera agrupacion con le fin de sacar una elemento y compararlos con todos los de la agrupacion siguiente
+            for b in templist2: # se recorre la segunda agrupacion para compararla completamente con el elemento de sacado anteiormente
+                if ((a.combinacion.count(1)+1 == b.combinacion.count(1)) or (a.combinacion.count(1) == b.combinacion.count(1)+1)):# se consulta si estos dos elemetos tienen un bit de diferencia
+                    tempbinary= list() # esta variable temporal se encargara de solamente guardar el binario
+                    tempdecimal=list()
+                    for c in range(len(a.combinacion)): # se comienzan a recorrer ambos elementos con el fin de encontrar la posicion donde se encuentra el uno en comun
+                        if a.combinacion[c] != b.combinacion[c]: # se consulta si precisamente la posisicon en cuestion contiene el cambio de bit
+                            tempbinary.append("-") # se registra el cambio de bit
                         else:
-                            tempbinary.append(a.combinacion[c])  # si no se guarda la posicion normal
-                    tempdecimal.append(a.decimal)  # se guarda el decimal del primer inplicante
-                    tempdecimal.append(b.decimal)  # se guarda el decimal del segundo inplicante
-                    ni = nodo(tempbinary, tempdecimal,
-                              0)  # se crea el objeto que guarda ambos datos tanto binarios como decimales
-                    tempbinary.clear()  # se vacia con el fin de evitar redundancia
+                            tempbinary.append(a.combinacion[c])# si no se guarda la posicion normal
+                    tempdecimal.append(a.decimal) # se guarda el decimal del primer inplicante
+                    tempdecimal.append(b.decimal) # se guarda el decimal del segundo inplicante
+                    ni=nodo(tempbinary,tempdecimal,0) # se crea el objeto que guarda ambos datos tanto binarios como decimales
+                    tempbinary.clear() # se vacia con el fin de evitar redundancia
                     tempdecimal.clear()
-                    tempcombinaciones.append(ni)  # se guarda en la agripacion correspondiente
-                    b.validacion = 1
-                    a.validacion = 1
+                    tempcombinaciones.append(ni)# se guarda en la agripacion correspondiente
+                    b.validacion=1
+                    a.validacion=1
+            
+        tempMcclauskeycomplete.append(tempcombinaciones)# se guarda las agrupaciones y se procede a los siguiente grupo
 
-        tempMcclauskeycomplete.append(
-            tempcombinaciones)  # se guarda las agrupaciones y se procede a los siguiente grupo
+    return tempMcclauskeycomplete
+
 
 
 if __name__ == "__main__":
-    numero_entradas = int(input(
-        "Introduce el numero de variables: ")) + 2  # En esta variable se guardaran el numero de variables o columnas
-    # Le sumo 2, porque en la primer columna, se almacenará el número en decimal desde [0-hasta el último bit] y la ultima contendra si la funcion es valida alli
-    filas = 2
-    filas = filas ** (numero_entradas - 2)  # Las filas serán igual a 2 elevado al numero de entradas
+    
+    numero_entradas =int(input("Introduce el numero de variables: "))+1       #En esta variable se guardaran el numero de variables o columnas
+    filas=2
+    filas = filas**(numero_entradas-1)       #Las filas serán igual a 2 elevado al numero de entradas
+    
+    tablaverdad=crear_matriz([],numero_entradas,filas)        #Llamo la funcion que me crea la tabla de verdad
+    
+    tablaverdad=llenar_matriz(filas, numero_entradas, tablaverdad) #se llena la matriz con los binarios 
 
-    tablaverdad = crear_matriz([], numero_entradas, filas)  # Llamo la funcion que me crea la tabla de verdad
+    matriz_nueva=minterms(tablaverdad,filas) # se solicitan los valores decimales donde la funcion es valida y se extraen sus valores binarios corresondientes
+    tablaverdad.clear()
+    #listMC=EMCclauskey(matriz_nueva,numero_entradas)
+    listMC=p(matriz_nueva,numero_entradas)
 
-    tablaverdad = llenar_matriz(filas, numero_entradas, tablaverdad)  # se llena la matriz con los binarios
-    print(
-        "--------------------------------------------------------------------------------------------------------------------")
-    dibujar_matriz(tablaverdad, numero_entradas, filas)
-    matriz_nueva = minterms(tablaverdad,
-                            filas)  # se solicitan los valores decimales donde la funcion es valida y se extraen sus valores binarios corresondientes
-    print(
-        "--------------------------------------------------------------------------------------------------------------------")
-    print("Los minterms son: ", matriz_nueva)
-    #dibujar_matriz(matriz_nueva, numero_entradas, filas)
-    # listMC=EMCclauskey(matriz_nueva,numero_entradas-1,filas)#primera agrupacion del maclauskey
+
+       
+    
